a9537c3591d1f57b8c5f5f2d599fc90c7033e3e1
Bruce <brucegeno34@gmail.com>
2025-11-22 22:17:05 +0200

    First commit to Bruce_Geno

--- src/_27193/q6/Account.java
+++ src/_27193/q6/Account.java
@@ -1,5 +1,20 @@
 package _27193.q6;
 
-public class Account {
-    
+public class Account extends Bank {
+    private String accountNumber;
+    private String accountType;
+    private double balance;
+
+    public Account(int id, java.time.LocalDate createdDate, java.time.LocalDate updatedDate,
+                   String bankName, String branchCode, String address,
+                   String accountNumber, String accountType, double balance) throws BkExceptions {
+        super(id, createdDate, updatedDate, bankName, branchCode, address);
+        if (balance < 0) throw new BkExceptions("Balance shouldn't  be negative (27193)");
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
 }
